8475a9343d51721534cc89998167c804bdd9dcb8
Ismet Hamzaj <81379437+IsmetHamzaj@users.noreply.github.com>
2022-02-17 15:19:41 +0100

    Add files via upload

--- controller/favoriteController.js
+++ controller/favoriteController.js
@@ -1,21 +1,40 @@
 const Favorite = require("../models/favoriteModel")
 const fetch = require('node-fetch2')
 
+
+
+
 const getFavorite = async (req, res, next) => {
     let model = await Favorite.findAll()
     try {
-        res.status(200).render('favorite', {
+        let data = {}
+        const {
+            heroid
+        } = req.hero;
+
+        if (heroid) {
+            data = await hero(req.hero.heroid)
+        }
+        await res.status(200).render('favorite', {
             title: "Favorites",
             para: "HI THIS IS THE FAVORITE",
-            model
+            model,
+            message: data?.message ,  data
         })
     } catch (err) {
         res.status(400).json({
             status: "Failed",
-            message: err.message
+            message: err.message,
         })
     }
 }
+
+const hero = async (hero) => await fetch(`https://www.superheroapi.com/api.php/10157652346894910/${hero}`)
+    .then(res => res.json())
+    .catch((err) => {
+        throw new Error('Failed to load Heroes')
+    })
+
 
 const removeFavorites = (req, res) => {
     let id = req.params.id
@@ -45,18 +64,17 @@ const removeFavorites = (req, res) => {
 const addFavorite = (req, res) => {
     Favorite.create({
             hero_id: req.body.hero_id,
+            // name: req.body.name,
+            // full_name: req.body.full_name,
+            // alignment: req.body.alignment,
+            // race: req.body.race,
+            // img: req.body.img
         })
         .then(() => {
-            res.status(200).json({
-                status: "Success",
-                message: "Hero added successfully",
-            })
+            res.redirect('/')
         })
         .catch((err) => {
-            res.status(400).json({
-                status: "Failed",
-                message: err.message,
-            });
+           res.redirect('/')
         });
 };
 
--- models/favoriteModel.js
+++ models/favoriteModel.js
@@ -7,7 +7,27 @@ const Favorite = db.define("favorites", {
         type: Sequelize.STRING,
         allowNull: false,
         unique: true
-    }
+    },
+    // name: {
+    //     type: Sequelize.STRING,
+    //     allowNull: true,
+    // },
+    // full_name: {
+    //     type: Sequelize.STRING,
+    //     allowNull: true
+    // },
+    // alignment: {
+    //     type: Sequelize.STRING,
+    //     allowNull: true
+    // },
+    // race: {
+    //     type: Sequelize.STRING,
+    //     allowNull: true
+    // },
+    // img: {
+    //     type: Sequelize.BLOB,
+    //     allowNull: true
+    // }
 })
 
 
